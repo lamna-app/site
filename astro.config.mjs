6ec7991e6f7df53404c23a0cbe5c608ac2eb06c9
cyrus01337 <cyrus01337@gmail.com>
2024-12-09 06:19:27 +0000

    Remove tail from URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import { defineConfig } from "astro/config";
 // TODO: Replace with Lamna domain when received
 // https://astro.build/config
 export default defineConfig({
+  base: "/",
   integrations: [vue(), tailwind()],
   server: {
     port: 3000,
